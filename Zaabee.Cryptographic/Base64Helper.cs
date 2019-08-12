68f90c78292b2610e508a410ba5f483fd669839d
Mutuduxf <aeondxf@live.com>
2019-08-12 14:40:35 +0800

    Recode

--- Zaabee.Cryptographic/Base64Helper.cs
+++ Zaabee.Cryptographic/Base64Helper.cs
@@ -5,27 +5,21 @@ namespace Zaabee.Cryptographic
 {
     public static class Base64Helper
     {
-        public static string ToBase64(this string str, Encoding encoding = null)
-        {
-            encoding = encoding ?? Encoding.UTF8;
-            return Convert.ToBase64String(encoding.GetBytes(str));
-        }
+        public static string ToBase64(this string str, Encoding encoding = null) =>
+            encoding is null
+                ? Convert.ToBase64String(Encoding.UTF8.GetBytes(str))
+                : Convert.ToBase64String(encoding.GetBytes(str));
 
-        public static string FromBase64(this string str, Encoding encoding = null)
-        {
-            encoding = encoding ?? Encoding.UTF8;
-            return encoding.GetString(Convert.FromBase64String(str));
-        }
+        public static string FromBase64(this string str, Encoding encoding = null) =>
+            encoding is null
+                ? Encoding.UTF8.GetString(Convert.FromBase64String(str))
+                : encoding.GetString(Convert.FromBase64String(str));
 
-        public static string ToBase64(this byte[] bytes)
-        {
-            return Convert.ToBase64String(bytes);
-        }
+        public static string ToBase64(this byte[] bytes) => Convert.ToBase64String(bytes);
 
-        public static string FromBase64(this byte[] bytes, Encoding encoding = null)
-        {
-            encoding = encoding ?? Encoding.UTF8;
-            return encoding.GetString(bytes);
-        }
+        public static string FromBase64(this byte[] bytes, Encoding encoding = null) =>
+            encoding is null
+                ? Encoding.UTF8.GetString(bytes)
+                : encoding.GetString(bytes);
     }
 }
