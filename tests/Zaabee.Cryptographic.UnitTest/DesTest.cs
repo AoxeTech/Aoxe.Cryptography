18826f016b9ac0d347f915bd5d825248d363e3bc
XiaoFeiDu <romanchaos@163.com>
2021-04-02 11:09:50 +0800

    Add DES and TripleDES support.

---
+++ tests/Zaabee.Cryptographic.UnitTest/DesTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace Zaabee.Cryptographic.UnitTest
+{
+    public class DesTest
+    {
+        [Fact]
+        public void Test()
+        {
+            DesHelper.Encoding = Encoding.UTF8;
+            Assert.Equal(DesHelper.Encoding, Encoding.UTF8);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the des key.", "Here is the des vector.", CipherMode.CBC)]
+        [InlineData("Here is some data to encrypt!", "Here is the des key.", "Here is the des vector.", CipherMode.ECB)]
+        public void DesStringTest(string original, string key, string vector, CipherMode cipherMode)
+        {
+            var encrypt = DesHelper.Encrypt(original, key, vector, cipherMode);
+            var decrypt = DesHelper.Decrypt(encrypt, key, vector, cipherMode);
+            Assert.Equal(original, decrypt);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the des key.", "Here is the des vector.", CipherMode.CBC)]
+        [InlineData("Here is some data to encrypt!", "Here is the des key.", "Here is the des vector.", CipherMode.ECB)]
+        public void DesBytesTest(string original, string key, string vector, CipherMode cipherMode)
+        {
+            var bKey = DesHelper.Encoding.GetBytes(key);
+            var bVector = DesHelper.Encoding.GetBytes(vector);
+            var encrypt = DesHelper.Encrypt(original, bKey, bVector, cipherMode);
+            var decrypt = DesHelper.Decrypt(encrypt, bKey, bVector, cipherMode);
+            Assert.Equal(original, decrypt);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the des key.", "01234567890123456789", new byte[] { })]
+        public void DesStringNullTest(string original, string key, string vector, byte[] encrypted)
+        {
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Encrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Encrypt(original, null, vector));
+
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Decrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Decrypt(encrypted, null, vector));
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", new byte[] { }, new byte[] { }, new byte[] { })]
+        public void DesByteNullTest(string original, byte[] key, byte[] vector, byte[] encrypted)
+        {
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Encrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Encrypt(original, null, vector));
+
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Decrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => DesHelper.Decrypt(encrypted, null, vector));
+        }
+    }
+}
---
+++ tests/Zaabee.Cryptographic.UnitTest/TripleDesTest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace Zaabee.Cryptographic.UnitTest
+{
+    public class TripleDesTest
+    {
+        [Fact]
+        public void Test()
+        {
+            TripleDesHelper.Encoding = Encoding.UTF8;
+            Assert.Equal(TripleDesHelper.Encoding, Encoding.UTF8);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the triple des key.", "Here is the triple des vector.",
+            CipherMode.CBC)]
+        [InlineData("Here is some data to encrypt!", "Here is the triple des key.", "Here is the triple des vector.",
+            CipherMode.ECB)]
+        public void TripleDesStringTest(string original, string key, string vector, CipherMode cipherMode)
+        {
+            var encrypt = TripleDesHelper.Encrypt(original, key, vector, cipherMode);
+            var decrypt = TripleDesHelper.Decrypt(encrypt, key, vector, cipherMode);
+            Assert.Equal(original, decrypt);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the triple des key.", "Here is the triple des vector.",
+            CipherMode.CBC)]
+        [InlineData("Here is some data to encrypt!", "Here is the triple des key.", "Here is the triple des vector.",
+            CipherMode.ECB)]
+        public void TripleDesBytesTest(string original, string key, string vector, CipherMode cipherMode)
+        {
+            var bKey = TripleDesHelper.Encoding.GetBytes(key);
+            var bVector = TripleDesHelper.Encoding.GetBytes(vector);
+            var encrypt = TripleDesHelper.Encrypt(original, bKey, bVector, cipherMode);
+            var decrypt = TripleDesHelper.Decrypt(encrypt, bKey, bVector, cipherMode);
+            Assert.Equal(original, decrypt);
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", "Here is the triple des key.", "01234567890123456789",
+            new byte[] { })]
+        public void TripleDesStringNullTest(string original, string key, string vector, byte[] encrypted)
+        {
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Encrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Encrypt(original, null, vector));
+
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Decrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Decrypt(encrypted, null, vector));
+        }
+
+        [Theory]
+        [InlineData("Here is some data to encrypt!", new byte[] { }, new byte[] { }, new byte[] { })]
+        public void TripleDesByteNullTest(string original, byte[] key, byte[] vector, byte[] encrypted)
+        {
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Encrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Encrypt(original, null, vector));
+
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Decrypt(null, key, vector));
+            Assert.Throws<ArgumentNullException>(() => TripleDesHelper.Decrypt(encrypted, null, vector));
+        }
+    }
+}
