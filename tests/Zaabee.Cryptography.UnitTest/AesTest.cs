c2a3c9c4d414ae659317fd889ba841f225f9ff53
Mutudu <aeondxf@live.com>
2023-04-04 10:19:31 +0800

    Remove odd namespace

--- tests/Zaabee.Cryptography.UnitTest/AesTest.cs
+++ tests/Zaabee.Cryptography.UnitTest/AesTest.cs
@@ -5,24 +5,24 @@ public class AesTest
     [Fact]
     public void GenerateKeyTest()
     {
-        var key0 = AES.AesHelper.GenerateKey();
-        var key1 = AES.AesHelper.GenerateKey();
+        var key0 = AesHelper.GenerateKey();
+        var key1 = AesHelper.GenerateKey();
         Assert.NotEqual(key0, key1);
     }
 
     [Fact]
     public void GenerateVectorTest()
     {
-        var vector0 = AES.AesHelper.GenerateVector();
-        var vector1 = AES.AesHelper.GenerateVector();
+        var vector0 = AesHelper.GenerateVector();
+        var vector1 = AesHelper.GenerateVector();
         Assert.NotEqual(vector0, vector1);
     }
 
     [Fact]
     public void GenerateKeyAndVectorTest()
     {
-        var (key0, vector0) = AES.AesHelper.GenerateKeyAndVector();
-        var (key1, vector1) = AES.AesHelper.GenerateKeyAndVector();
+        var (key0, vector0) = AesHelper.GenerateKeyAndVector();
+        var (key1, vector1) = AesHelper.GenerateKeyAndVector();
         Assert.NotEqual(key0, key1);
         Assert.NotEqual(vector0, vector1);
     }
@@ -32,7 +32,7 @@ public class AesTest
     public void AesBytesTest(string original)
     {
         var originalBytes = Encoding.UTF8.GetBytes(original);
-        var (key, vector) = AES.AesHelper.GenerateKeyAndVector();
+        var (key, vector) = AesHelper.GenerateKeyAndVector();
         var encrypt = originalBytes.EncryptByAes(key, vector);
         var decrypt = encrypt.DecryptByAes(key, vector);
         var decryptString = Encoding.UTF8.GetString(decrypt);
@@ -44,7 +44,7 @@ public class AesTest
     public void AesStreamTest(string original)
     {
         var originalStream = original.GetUtf8Bytes().ToMemoryStream();
-        var (key, vector) = AES.AesHelper.GenerateKeyAndVector();
+        var (key, vector) = AesHelper.GenerateKeyAndVector();
         var encryptStream = originalStream.EncryptByAes(key, vector);
         var decryptStream = encryptStream.DecryptByAes(key, vector);
         var decryptString = decryptStream.ReadToEnd().GetStringByUtf8();
@@ -56,7 +56,7 @@ public class AesTest
     public async Task AesStreamAsyncTest(string original)
     {
         var originalStream = original.GetUtf8Bytes().ToMemoryStream();
-        var (key, vector) = AES.AesHelper.GenerateKeyAndVector();
+        var (key, vector) = AesHelper.GenerateKeyAndVector();
         var encryptStream = await originalStream.EncryptByAesAsync(key, vector);
         var decryptStream = await encryptStream.DecryptByAesAsync(key, vector);
         var decryptString = (await decryptStream.ReadToEndAsync()).GetStringByUtf8();
