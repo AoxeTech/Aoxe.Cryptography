0b64335e9f2f2a2828f9a97bb531ad3bf3193db6
Mutudu <aeondxf@live.com>
2023-09-03 16:46:54 +0800

    Upgrade

--- tests/Zaabee.Cryptography.UnitTest/CommonHelper.cs
+++ tests/Zaabee.Cryptography.UnitTest/CommonHelper.cs
@@ -2,11 +2,12 @@ namespace Zaabee.Cryptography.UnitTest;
 
 public static class CommonHelper
 {
-    internal static IEnumerable<byte> HexToBytes(string str)
+    internal static IEnumerable<byte> HexToBytes(string hex)
     {
-        var arr = str.Split('-');
-        var array = new byte[arr.Length];
-        for (var i = 0; i < arr.Length; i++) array[i] = Convert.ToByte(arr[i], 16);
-        return array;
+        var numberChars = hex.Length;
+        var bytes = new byte[numberChars / 2];
+        for (var i = 0; i < numberChars; i += 2)
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        return bytes;
     }
 }
